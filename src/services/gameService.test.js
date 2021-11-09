fbf1dc81e92246a53c74130b0f5e0a4a19a42808
loleseur <loleseur@a112pc15L.local.isima.fr>
2021-11-09 13:46:19 +0100

    Add tests for gameService.takeGood()

--- src/services/gameService.test.js
+++ src/services/gameService.test.js
@@ -46,3 +46,46 @@ describe("Game service", () => {
     expect(game._players[1].camelsCount).toBe(0)
   })
 })
+
+describe("Test takeGood", () => {
+  function baseGame() {
+    return {
+      _deck: ["diamonds"],
+      market: ["cloth"],
+      _players: [{ hand: [], camelsCount: 0 }],
+      currentPlayerIndex: 0,
+    }
+  }
+
+  test("Normal call", () => {
+    const game = baseGame()
+    gameService.takeGood(game, 0, "cloth")
+    expect(game.market).toStrictEqual(["diamonds"])
+    expect(game._players[0].hand).toStrictEqual(["cloth"])
+    expect(game._deck).toStrictEqual([])
+  })
+
+  test("Bad player id", () => {
+    const game = baseGame()
+    game.currentPlayerIndex = 1
+    expect(gameService.takeGood.bind(null, game, 0, "foo")).toThrow()
+  })
+
+  test("Too many cards in hand", () => {
+    const game = baseGame()
+    game._players[0].hand = Array(7).fill("cloth")
+    expect(gameService.takeGood.bind(null, game, 0, "foo")).toThrow()
+  })
+
+  test("Lots of camels in hand", () => {
+    const game = baseGame()
+    game._players[0].hand = Array(7).fill("camel")
+    gameService.takeGood(game, 0, "cloth")
+    expect(game._players[0].hand.indexOf("cloth")).toBeDefined()
+  })
+
+  test("Good not found", () => {
+    const game = baseGame()
+    expect(gameService.takeGood.bind(null, game, 0, "missing")).toThrow()
+  })
+})
